f4e95d10ff67f001849429de0509d382195892c2
Matt Vu <107937474+tmattvu@users.noreply.github.com>
2024-04-25 17:34:55 -0400

    cooked; frontend framework done (#2)
    Co-authored-by: Matt Vu <tmattvu@ad.unc.edu>

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from '/workspaces/genie-dating-app/frontend/src/app/app.routing.module'; 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component'; 
+import { Profile } from './home/widgets/profile/profile.widget';
+import { Matches } from './home/widgets/matches/matches.widget';
+import { Gallery } from './home/widgets/gallery/gallery.widget';
+import { LoginComponent } from '/workspaces/genie-dating-app/frontend/src/app/login-screen/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent 
+    LoginComponent,
+    HomeComponent,
+    CreateAccountComponent,
+    Profile,
+    Matches,
+    Gallery 
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule 
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Matches } from './widgets/matches/matches.widget';
+import { Gallery } from './widgets/gallery/gallery.widget';
+import { Profile } from './widgets/profile/profile.widget';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [MatCardModule],
+  standalone: true
 })
 export class HomeComponent {
-
+ constructor(){}
 }
